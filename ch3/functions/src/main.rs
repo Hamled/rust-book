75bdca98895eb8663825ae900aadcdad3c555897
Charles Ellis <hamled@hamled.name>
2017-03-29 23:16:17 -0700

    Use block in let statement

--- ch3/functions/src/main.rs
+++ ch3/functions/src/main.rs
@@ -1,3 +1,11 @@
 fn main() {
-    let y = 6;
+    let x = 5;
+
+    let y = {
+        let x = 3;
+        x + 1
+    };
+
+    println!("The value of x is: {}", x);
+    println!("The value of y is: {}", y);
 }
