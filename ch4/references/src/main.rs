a961457f2da46df1d798cb8631028051aa38278b
Charles Ellis <hamled@hamled.name>
2017-04-23 19:53:36 -0700

    Mutable references

--- ch4/references/src/main.rs
+++ ch4/references/src/main.rs
@@ -1,9 +1,15 @@
 fn main() {
-    let s1 = String::from("hello");
+    let mut s1 = String::from("hello");
+
+    change(&mut s1);
 
     let len = calculate_length(&s1);
 
     println!("The length of '{}' is {}", s1, len);
+}
+
+fn change(a_string: &mut String) {
+    a_string.push_str(", world!");
 }
 
 fn calculate_length(a_string: &String) -> usize {
